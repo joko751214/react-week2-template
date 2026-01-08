6c2ea7175267fda1c2352c777c091553557ab7a4
Jin <jin@JindeMacBook-Pro.local>
2026-01-08 10:09:54 +0800

    axios 集成設置、Vite config 設置、登入頁面與產品頁面功能實作

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const API_URL = env.VITE_API_BASE_URL;
+  return {
+    plugins: [react()],
+    base: '/react-week2-template/',
+    server: {
+      host: '0.0.0.0',
+      port: 8080,
+      open: true,
+      proxy: {
+        '/api': {
+          target: API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(new RegExp(`^/api`), API_URL),
+        },
+      },
+    },
+  };
+});
